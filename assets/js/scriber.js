37d89c4fb42c450919a897a0b0eb67bc0c7a149a
sadainiqrar <sadain.abbasi@gmail.com>
2018-07-08 18:18:23 +0500

    feedback

--- assets/js/scriber.js
+++ assets/js/scriber.js
@@ -1,3 +1,60 @@
-$(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+$(window).scroll(function() {
+  $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
 });
+
+
+
+
+
+
+function clicked(el) {
+	
+  var element = el.srcElement.closest(".tile");
+  console.log(element.id)
+  var res = element.id.split("-");
+  var key = res[1];
+  var section = res[0];
+
+  var d = document.getElementById(section);
+	d.classList.remove("make-visible");
+  
+	d.classList.add("hide");
+  
+  var siblings = [];
+    var sibling = element.parentNode.firstChild;
+    var skipMe = element;
+    for ( ; sibling; sibling = sibling.nextSibling ) 
+       if ( sibling.nodeType == 1)
+          siblings.push( sibling );
+  console.log(siblings.length)
+  console.log(siblings[siblings.length-1])
+  var total = siblings.length - 1
+  var last_key = siblings[siblings.length-1]
+
+  var offset = element.offsetLeft;
+  console.log(res)
+  var next = Math.ceil(key/6)*6;
+  console.log(last_key)
+  var ending_element = last_key
+  if(next <= total){
+	console.log('if')
+	  ending_element = document.getElementById(section + '-' + next);
+  }
+	else{console.log('else')
+	ending_element = last_key;
+	}
+	console.log(ending_element)
+  
+  var before = document.getElementById('before');
+  
+  var after = document.getElementById('after');
+  ending_element.parentNode.insertBefore(d, ending_element.nextSibling);
+  //document.styleSheets[0].addRule('description:before','left: "'+offset+'px";');
+  before.setAttribute('style', 'left:'+ offset + 'px;');
+  after.setAttribute('style', 'left:'+ offset + 'px;');
+  
+  d.classList.add("make-visible");
+
+  d.classList.remove("hide");
+  
+}
