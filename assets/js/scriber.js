f924665bd8cfcc73273c80ccdc375996b6ab0fc5
sadainiqrar <sadain.abbasi@gmail.com>
2018-07-28 02:12:53 +0500

    fixed issues

--- assets/js/scriber.js
+++ assets/js/scriber.js
@@ -111,42 +111,10 @@ function submitSearch() {
   var submit = document.getElementById("search");
   submit.classList.add("hide");
   submit.classList.remove("make-visible");
+  window.location.href='search.html'
 }
 
 function smoothScroll(elementId) {
-  var MIN_PIXELS_PER_STEP = 16;
-  var MAX_SCROLL_STEPS = 30;
-  var target = document.getElementById(elementId);
-  var scrollContainer = target;
-  do {
-    scrollContainer = scrollContainer.parentNode;
-    if (!scrollContainer) return;
-    scrollContainer.scrollTop += 1;
-  } while (scrollContainer.scrollTop == 0);
-
-  var targetY = 0;
-  do {
-    if (target == scrollContainer) break;
-    targetY += target.offsetTop - 28;
-  } while ((target = target.offsetParent));
-
-  var pixelsPerStep = Math.max(
-    MIN_PIXELS_PER_STEP,
-    (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS
-  );
-
-  var stepFunc = function() {
-    scrollContainer.scrollTop = Math.min(
-      targetY,
-      pixelsPerStep + scrollContainer.scrollTop
-    );
-
-    if (scrollContainer.scrollTop >= targetY) {
-      return;
-    }
-
-    window.requestAnimationFrame(stepFunc);
-  };
-
-  window.requestAnimationFrame(stepFunc);
+  var elmnt = document.getElementById(elementId);
+  elmnt.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
 }
